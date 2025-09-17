17f70259d4d2e420df0347fd1c7055c93e7d741b
AlexandreFigueired0 <alexandrerafaof@gmail.com>
2025-09-17 17:24:41 +0100

    Script to get stocks from sec api instead of companies.csv

---
+++ utils/test_db.py
@@ -0,0 +1,20 @@
+import psycopg2
+import pandas as pd
+import os
+
+conn = psycopg2.connect(
+    dbname=os.getenv("POSTGRES_DBNAME"),
+    user=os.getenv("POSTGRES_USERNAME"),
+    password=os.getenv("POSTGRES_PASSWORD"),
+    host=os.getenv("POSTGRES_HOST"),
+    port='5432'
+)
+
+cursor = conn.cursor()
+
+
+# Get entries of subscribed_stock table
+cursor.execute('SELECT * FROM stocks')
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
